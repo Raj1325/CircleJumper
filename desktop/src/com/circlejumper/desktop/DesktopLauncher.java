54afb87098f7cb4b065916e7b04671750f73b598
Raj Dighe <rppdighe4892@gmail.com>
2020-10-30 12:48:23 +1100

    Project setup

---
+++ desktop/src/com/circlejumper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.circlejumper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.circlejumper.CircleJumper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CircleJumper(), config);
+	}
+}
---
+++ ios/src/com/circlejumper/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.circlejumper;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.circlejumper.CircleJumper;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new CircleJumper(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
